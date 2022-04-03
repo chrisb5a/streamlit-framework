eb95dc60e5f35429da13f6d877ff39535aaa37f4
chrisb5a <crbeynis@buffalo.edu>
2022-04-03 19:45:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.title('sentiment vs stock')
 #inputs = st.selectbox("Do you care about the stock or sentiment?", ("stock", "sentiment")) 
 #inputs = st.text_input("Do you care about the stock or sentiment?")
 st.text_input("Do you care about the stock or sentiment?", key = "choice")
-st.write('ok', st.session_state.choice == 'sentiment', st.session_state.choice)
+#st.write('ok', st.session_state.choice == 'sentiment', st.session_state.choice)
 
-if str(st.session_state.choice) == 'stock' :
+if st.session_state.choice == 'stock' :
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
     CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo'
 
@@ -19,11 +19,15 @@ if str(st.session_state.choice) == 'stock' :
         cr = csv.reader(decoded_content.splitlines(), delimiter=',')
         my_list = list(cr)
         for row in my_list:
-            print(row)
+            st.write(row)
+  else:
 
-if str(st.session_state.choice) == 'sentiment' :
-    url = 'https://www.alphavantage.co/query?function=CONSUMER_SENTIMENT&apikey=demo'
-    r = requests.get(url)
-    data = r.json()
+    if str(st.session_state.choice) == 'sentiment' :
+        url = 'https://www.alphavantage.co/query?function=CONSUMER_SENTIMENT&apikey=demo'
+        r = requests.get(url)
+        data = r.json()
 
-    print(data)
+        st.write(data)
+     else:
+        
+        st.write('write sentiment or stock!')
