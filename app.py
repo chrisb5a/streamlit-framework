ca0c34cfbaf93f39563f5f2f454d20c715ba98c0
chrisb5a <crbeynis@buffalo.edu>
2022-04-03 21:43:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ else:
         #datas['x'] = datas['x'].dt.strftime('%Y-%m-%d')
         datas['x1'] = datas['x1'].apply(lambda x: pd.Timestamp(x).strftime('%Y-%m-%d'))
         c = alt.Chart(datas.reset_index()).mark_line().encode(
-        x='index:T',
+        x1='index:T',
         y='value:Q')
         
         st.altair_chart(datas, use_container_width = True)
